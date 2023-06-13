ef775183d8cae2b734d0b8265945b201f40b4f29
GitMarkPaul <markpaulcaneda1997@gmail.com>
2023-06-13 14:13:40 +0800

    add smooth scroll padding

--- src/js/header.js
+++ src/js/header.js
@@ -26,5 +26,70 @@ export default function Header() {
 
         const navMenuElement = document.querySelector('.nav-menu');
         navMenuElement.classList.toggle('show');
+
     });
+    const navLinks = document.querySelectorAll('.nav-link');
+
+    navLinks.forEach((link) => {
+        link.addEventListener('click', () => {
+            document.querySelector('.nav-menu').classList.toggle('show');
+            document.querySelector('.hamburger-menu').classList.toggle('active');
+        });
+    });
+    
+    const navigationLinks = document.querySelectorAll('.nav-link');
+    const navigationHeight = document.querySelector('.nav-wrapper').offsetHeight;
+
+    // Function to update active navigation link
+    const updateActiveLink = () => {
+    const currentScrollPos = window.scrollY;
+
+    // Loop through each section and find the one in view
+    document.querySelectorAll('section').forEach(section => {
+        const sectionTop = section.offsetTop;
+        const sectionHeight = section.offsetHeight;
+
+        if (currentScrollPos >= sectionTop - navigationHeight && currentScrollPos < sectionTop + sectionHeight) {
+        // Get the ID of the section in view
+        const sectionId = section.getAttribute('id');
+
+        // Loop through navigation links and update the active link
+        navigationLinks.forEach(link => {
+            const linkHref = link.getAttribute('href').substring(1);
+            if (linkHref === sectionId) {
+            link.setAttribute('aria-current', 'page');
+            } else {
+            link.removeAttribute('aria-current');
+            }
+        });
+        }
+    });
+    };
+
+    // Add click event listener to each navigation link
+    navigationLinks.forEach(link => {
+    link.addEventListener('click', function(event) {
+        event.preventDefault(); // Prevent the default link behavior
+
+        const targetId = this.getAttribute('href').substring(1); // Get the target element's ID
+        const targetElement = document.getElementById(targetId); // Find the target element
+
+        if (targetElement) {
+        const targetOffsetTop = targetElement.offsetTop;
+
+        // Adjust the scroll position, considering the navigation height
+        window.scrollTo({
+            top: targetOffsetTop - navigationHeight,
+            behavior: 'smooth'
+        });
+        }
+    });
+    });
+
+    // Update active link on initial page load
+    updateActiveLink();
+
+    // Update active link on scroll
+    window.addEventListener('scroll', updateActiveLink);
+
 }
