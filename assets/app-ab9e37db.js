07a2ee5c691ad5a46b1953bfb49b438c263152d7
GitMarkPaul <markpaulcaneda1997@gmail.com>
2023-06-13 11:33:48 +0800

    Updates

---
+++ assets/app-ab9e37db.js
@@ -0,0 +1,1 @@
+import{a,B as p,G as s,T as e,H as t}from"./header-306a3cf7.js";a.init();document.addEventListener("turbo:load",(()=>{p.apply(p),s.apply(s),e.apply(e),t.apply(t)}));
--- assets/app-cd3400e1.js
+++
@@ -1,1 +0,0 @@
-import{B as a,G as p,T as e,H as s}from"./header-97bda12d.js";document.addEventListener("turbo:load",(()=>{a.apply(a),p.apply(p),e.apply(e),s.apply(s)}));
