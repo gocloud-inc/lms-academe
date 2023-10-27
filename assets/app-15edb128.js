fbcd7b9ecbac2224909cbe1de43b57599ae1495a
GitMarkPaul <markpaulcaneda1997@gmail.com>
2023-10-27 10:38:25 +0800

    Updates

---
+++ assets/app-15edb128.js
@@ -0,0 +1,1 @@
+import{a,B as p,G as s,T as t,H as l,b as o}from"./contact-cb119a61.js";a.init();document.addEventListener("turbo:load",(()=>{p.apply(p),s.apply(s),t.apply(t),l.apply(l),o.apply(o)}));
