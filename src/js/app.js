bb94bb852844fd1af8b0a5a8f298fa9d4e8554c9
GitMarkPaul <markpaulcaneda1997@gmail.com>
2023-10-27 10:38:54 +0800

    add function to contact form

--- src/js/app.js
+++ src/js/app.js
@@ -11,12 +11,14 @@ import Button from "./buttons";
 import Greeting from "./greeting";
 import Theme from "./theme";
 import Header from "./header";
+import Contact from "./contact";
 
 const turboLoad = () => {
     Button.apply(Button);
     Greeting.apply(Greeting);
     Theme.apply(Theme);
     Header.apply(Header);
+    Contact.apply(Contact);
 }
 
 document.addEventListener("turbo:load", turboLoad);
